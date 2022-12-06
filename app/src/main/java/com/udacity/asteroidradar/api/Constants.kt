b82071f5bd9bb997f62c333a0845dd04efb644c0
toaderDragos <toader.dragosh@gmail.com>
2022-12-06 11:28:55 +0200

    Almost complete but it crashes without a clear message if it doesn't have internet. No filters yet.

--- app/src/main/java/com/udacity/asteroidradar/api/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val YOUR_API_KEY = ""
+    const val YOUR_API_KEY = "5FsRP4Q0Vod1JwLfXcMbZm6y5fFeIBeg1anS3NBd"
 }
